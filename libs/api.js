1f514f5b34bb98d3ac4caa1397da475c4a1f92cd
Tridots <info@tridotstech.com>
2025-02-24 18:51:52 +0530

    24-02-2025

--- libs/api.js
+++ libs/api.js
@@ -614,7 +614,7 @@ export async function get_product_details(code) {
     }
     let api = `https://${domain}/api/method/igh_search.igh_search.api.get_product_info?item_code=${code}`
     const myHead = new Headers({ "Authorization": `token ${apikey}:${secret}`, "Content-Type": "application/json","x-typesense-api-key": "xyz" })
-    const response = await fetch(api, { method: 'GET', headers: myHead, })
+    const response = await fetch(api, { method: 'POST', headers: myHead, })
     return await response.json()
 }
 
